1f98b4ea1ec647e5f036452e76968aa03ef44285
cobrianz <briancheruiyot.h@gmail.com>
2025-02-09 13:23:59 +0300

    update

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
+const open_menu = document.getElementById("open_menu");
+const close_menu = document.getElementById("close_menu");
+const menu = document.querySelector(".menu ul");
+// Swiper slider
 document.addEventListener("DOMContentLoaded", function () {
     new Swiper(".swiper", {
         loop: true, // Infinite loop
         autoplay: {
             delay: 3000, // 3 seconds
-            disableOnInteraction: false, // Keep autoplay after user interaction
-        },
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
+            disableOnInteraction: false, 
         },
         pagination: {
             el: ".swiper-pagination",
             clickable: true,
         },
-        effect: "coverflow", // Other effects: "fade", "cube", "coverflow"
+        effect: "coverflow", 
     });
 });
+
+
+
+function openMenu(){
+    menu.style.display = "flex";
+    open_menu.style.display = "none";
+    close_menu.style.display = "flex";
+}
+
+function closeMenu() {
+    menu.style.display = "none";
+    open_menu.style.display = "flex";
+    close_menu.style.display = "none";
+}
